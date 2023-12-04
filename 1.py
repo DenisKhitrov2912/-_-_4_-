c343daa93c69cb9919ce36c35f7374e73897ae11
Khitrov <Hitrov.95@yandex.ru>
2023-12-04 19:28:16 +0300

    part of main code

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def get_api_data(api_url, params=None, limit=None):
     # Опционально добавьте параметры запроса, если они требуются
     if params is None:
@@ -13,10 +14,35 @@ def get_api_data(api_url, params=None, limit=None):
     if response.status_code == 200:
         # Разберите JSON-ответ
         data = response.json()
+        keys = ["items"]
+        filtered_data = {k: data[k] for k in keys}
+        counter = 0
+        while True:
+            for v in filtered_data["items"]:
+                if counter < limit:
+                    counter += 1
+                    if v['salary'] is None:
+                        salary = "Зарплата не указана"
+                        salary_to = ''
+                        salary_currency = ''
+                    if v['salary'] is not None:
+                        if v['salary']['from'] is None:
+                            salary = ''
+                        else:
+                            salary = f"от {v['salary']['from']}"
+                        if v['salary']['to'] is None:
+                            salary_to = ''
+                        else:
+                            salary_to = f"до {v['salary']['to']}"
+                        salary_currency = v['salary']['currency']
+                    if v["address"] is None:
+                        city_address = "Город не указан"
+                    else:
+                        city_address = v["address"]["city"]
+                    print(v['id'], v['name'], salary, salary_to, salary_currency, v['alternate_url'], city_address, v['employer']['name'], v['employer']['alternate_url'])
+            else:
+                break
 
-        # Примените ограничение по количеству результатов, если указано
-        if limit is not None:
-            data = data[:limit]
 
         # Сохраните данные в JSON-файл
         with open('api_data.json', 'w', encoding='utf-8') as json_file:
@@ -26,10 +52,17 @@ def get_api_data(api_url, params=None, limit=None):
     else:
         print(f"Ошибка при выполнении запроса. Код: {response.status_code}")
 
+
 # Пример использования
 api_url = 'https://api.hh.ru/vacancies'
-words = input()
+words = input("Поиск по профессиям: ")
 params = {'text': words}
-limit = None  # Укажите желаемое ограничение по количеству результатов
+while True:
+    try:
+        limit = int(input("Введите количество результатов. Максимальное число результатов на странице - 20 "))
+        break
+    except ValueError:
+        print("Введите число!")
+
 
 get_api_data(api_url, params=params, limit=limit)
